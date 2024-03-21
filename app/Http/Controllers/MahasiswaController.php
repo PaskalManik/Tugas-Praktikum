1cb86b66671b3b445504237de551f90d03dc4785
PaskalManik <paskalirvaldi10@gmail.com>
2024-03-21 11:41:13 +0700

    first commit

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
 
 class MahasiswaController extends Controller
 {
@@ -11,7 +12,10 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return view('mahasiswa');
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa', [
+            'mahasiswa' => $mahasiswa
+        ]);
     }
 
     /**
